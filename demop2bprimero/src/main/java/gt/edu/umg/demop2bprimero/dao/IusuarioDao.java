c1d695ca6cdd0e2588bf56e29a7ecc60ad866c01
JoseEOrdoneze <89282033+JoseEOrdoneze@users.noreply.github.com>
2021-11-09 22:01:16 -0600

    Add files via upload

---
+++ demop2bprimero/src/main/java/gt/edu/umg/demop2bprimero/dao/IusuarioDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.umg.demop2bprimero.dao;
+
+import gt.edu.umg.demop2bprimero.Usuarios;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public interface IusuarioDao extends CrudRepository<Usuarios, Integer> {
+    
+    
+    
+    
+    
+    
+}
